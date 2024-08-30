25662373a3984a297983dd089f4083fc4d81d8ef
githarshhh <soul.always.op@gmail.com>
2024-08-30 20:35:53 +0530

    Leetcode 83

---
+++ Leetcode/Leetcode_83_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,16 @@
+public class Leetcode_83_RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while(curr != null && curr.next != null){
+            if(curr.val == curr.next.val){
+                curr.next = curr.next.next;
+            }
+            else{
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+}
