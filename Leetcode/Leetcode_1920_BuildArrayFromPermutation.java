0e8452e2662111539ca0e852f02b89789807fa81
githarshhh <soul.always.op@gmail.com>
2024-09-01 21:35:53 +0530

    Leetcode 1920

---
+++ Leetcode/Leetcode_1920_BuildArrayFromPermutation.java
@@ -0,0 +1,11 @@
+public class Leetcode_1920_BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int ans[]= new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
