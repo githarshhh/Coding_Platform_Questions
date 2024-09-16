7a0e83d3705cf7717406da52ff614bf74f029356
githarshhh <soul.always.op@gmail.com>
2024-09-16 21:49:23 +0530

    Leetcode 147

---
+++ Leetcode/Leetcode_147_InsertionSortList.java
@@ -0,0 +1,21 @@
+public class Leetcode_147_InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        ListNode curr = head;
+
+        while(curr != null){
+            pq.add(curr.val);
+            curr = curr.next;
+        }
+
+        curr = head;
+
+        while(curr != null){
+            curr.val = pq.poll();
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
