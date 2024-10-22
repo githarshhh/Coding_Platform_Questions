8da3087463831b42c3b6fbe9f6f6b48f9833f91e
githarshhh <soul.always.op@gmail.com>
2024-10-22 18:55:24 +0530

    Leetcode 226

---
+++ Leetcode/Leetcode_226_InvertBinaryTree.java
@@ -0,0 +1,14 @@
+import javax.swing.tree.TreeNode;
+
+public class Leetcode_226_InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
