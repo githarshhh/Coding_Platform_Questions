bc792aa1ef1857c6b2f9e6b059483dfe3ced3094
githarshhh <soul.always.op@gmail.com>
2024-10-26 12:50:39 +0530

    Leetcode 98

---
+++ Leetcode/Leetcode_98_ValidateBinarySearchTree.java
@@ -0,0 +1,12 @@
+public class Leetcode_98_ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean helper(TreeNode node, long min, long max){
+        if(node == null) return true;
+        else if(node.val <= min || node.val >= max) return false;
+
+        return helper(node.left, min, node.val) && helper(node.right, node.val, max); 
+    }
+}
